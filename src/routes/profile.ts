45dde1d559dd3450df3b6656c8cf07d6b8e71115
Julian <julian.beutin@viacesi.fr>
2021-06-25 02:12:00 +0200

    :adhesive_bandage: changing validator route put

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -54,9 +54,25 @@ router.get(
 router.put(
   "/:id",
 
-  body("name").escape().isLength({ min: 1, max: 50 }),
+  body("firstName").escape().isLength({ min: 1, max: 50 }),
 
-  body("description").escape().isLength({ min: 0, max: 255 }),
+  body("lastName").escape().isLength({ min: 1, max: 50 }),
+
+  body("phoneNumber").escape().isNumeric().isMobilePhone("fr-FR"),
+
+  body("street").escape().isLength({ min: 1, max: 50 }),
+
+  body("city").escape().isLength({ min: 1, max: 50 }),
+
+  body("state").escape().isLength({ min: 1, max: 50 }),
+
+  body("zip").escape().isNumeric(),
+
+  body("sponsorCode").escape().isLength({ min: 0, max: 50 }).notEmpty(),
+
+  body("sponsor").escape().isLength({ min: 0, max: 50 }),
+
+  body("userID").escape().isNumeric(),
 
   profileController.updateProfile
 );
